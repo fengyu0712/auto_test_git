97b01e3bd21b667c0663abf35189847d0154d44f
ex_zhongyq <ex_zhongyq@partner.midea.com>
2021-04-27 16:11:47 +0800

    恢复jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,10 @@
 @Library('aicloud-pipeline-share-lib') _
 import cn.aimidea.cd.pipeline.lib.*
-String APP_VER = ""
 
 pipeline {
     agent { label "docker" }
     triggers {
         gitlab(triggerOnPush: true, triggerOnMergeRequest: true, branchFilterType: 'All', secretToken: "246188d61ddf8a33355c4100a425187b")
-        //分钟，小时，日，月，周
-        cron('0 2 * * *')  //表示每天凌晨2点执行一次
     }
     options {
         gitLabConnection('mideagitlab')
@@ -19,20 +16,21 @@ pipeline {
                 script{
                     // 初始化流水线全局变量，必须
                     aiInitGlobalEnv()
-                    APP_VER = aiVerGenerate()
+
                     // 打印环境变量
                     sh "printenv"
                 }
             }
         }
         stage('build') {
-//            when {
-//                expression {
-//                    return env.gitlabSourceBranch == "master" && env.gitlabActionType == "PUSH"
-//                }
-//            }
+            when {
+                expression {
+                    return env.gitlabSourceBranch == "master" && env.gitlabActionType == "PUSH"
+                }
+            }
             steps {
                 script {
+                    APP_VER = aiVerGenerate()
 
                     docker.withRegistry("${env.ALIYUN_DOCKER_REGISTRY_URL}", "${env.ALIYUN_DOCKER_REGISTRY_ACCOUNT}") {
                         def imageTag = "registry-vpc.cn-hangzhou.aliyuncs.com/midea-aiplatform/speech-test:${APP_VER}"
